b79b259e3a24298a8438ed9bfbef410baa46303f
Vinícius Pavão <viniciuspavao2004@gmail.com>
2025-03-11 12:35:32 -0300

    Update photometer.ino

--- photometer/photometer.ino
+++ photometer/photometer.ino
@@ -1,5 +1,5 @@
 /*
-    Photometer.ino
+    Photometer.ino - Code for the Arduino-based phototrigger
     Copyright (C) 2024  Vinicius Pavao
 
     This program is free software: you can redistribute it and/or modify
@@ -15,3 +15,27 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
+int l;
+int photo = A0;
+int out = 2;
+ 
+void setup() {
+    Serial.begin(9600);
+    pinMode(photo, INPUT);
+    pinMode(out, OUTPUT);
+    digitalWrite(out, LOW);
+}
+
+void loop() {
+    l = analogRead(photo);
+    //Serial.print(float((5000*l)/1024)); Serial.println(" mV");
+    delay(1);
+    if((analogRead(photo) - l) > 15) {
+        //Serial.println(">> Trigger signal sent");
+        digitalWrite(out, HIGH);
+        delay(200);
+        digitalWrite(out, LOW);
+        delay(1000);
+    }
+}
